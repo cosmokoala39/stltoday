7c626d57e21c7e3e672c0ae0d556f0666047fd64
cosmo <cosmokoala39@gmail.com>
2025-07-08 15:45:02 +0530

    updatedd

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    
+      domains:['bloximages.newyork1.vip.townnews.com']
+    
+  }
 };
 
 export default nextConfig;
